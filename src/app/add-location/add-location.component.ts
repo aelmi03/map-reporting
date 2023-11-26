949eaaf655bdad5059cbd8bc43aa50134f0aca85
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2023-11-25 18:58:30 -0800

    Add functionality to display location modal when clicked and to be able to go back to main form

--- src/app/add-location/add-location.component.ts
+++ src/app/add-location/add-location.component.ts
@@ -9,16 +9,18 @@ import { Validators, FormGroup, FormControl } from '@angular/forms';
 export class AddLocationComponent {
  form:FormGroup;
  @Output() addLocation = new EventEmitter<Location>();
-
+@Output() hideModal = new EventEmitter();
  constructor(){
   let formControls = {
     latitude: new FormControl('',[
       Validators.required,
-      Validators.minLength(2)
+      Validators.minLength(2),
+      Validators.pattern(/^-?([1-8]?\d(\.\d+)?|90(\.0+)?)$/)
     ]),
     longitude: new FormControl('',[
       Validators.required,
-      Validators.minLength(2)
+      Validators.minLength(2),
+      Validators.pattern(/^-?((1[0-7]\d(\.\d+)?|180(\.0+)?)|(\d{1,2}(\.\d+)?))$/)
     ]),
     placeName: new FormControl('',[
       Validators.required,
@@ -30,5 +32,8 @@ export class AddLocationComponent {
 onSubmit(value:Location){
   this.addLocation.emit(value);
   this.form.reset();
+}
+closeModal(){
+  this.hideModal.emit();
 }
 }
--- src/app/create-report-form/create-report-form.component.ts
+++ src/app/create-report-form/create-report-form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {FormGroup, FormControl, Validators, AbstractControl} from '@angular/forms';
 import NuisanceReport from '../types/nuisance_report';
-
+import { Location } from '../types/nuisance_report';
 @Component({
   selector: 'app-create-report-form',
   templateUrl: './create-report-form.component.html',
@@ -9,7 +9,11 @@ import NuisanceReport from '../types/nuisance_report';
 })
 export class CreateReportFormComponent {
   form:FormGroup;
+  showLocationModal:boolean
+  selectedLocation:Location
+  locations:Location[]
   constructor(){
+    
     let formControls = {
       reportingPerson: new FormControl('',[
         Validators.required,
@@ -34,8 +38,16 @@ export class CreateReportFormComponent {
       phoneNumber:new FormControl('', [
         this.phoneNumberValidator
       ] )
-      
     }
+     this.locations= [
+      { placeName: 'New York', latitude: 40.7128, longitude: -74.0060 },
+      { placeName: 'London', latitude: 51.5074, longitude: -0.1278 },
+      { placeName: 'Paris', latitude: 48.8566, longitude: 2.3522 },
+      { placeName: 'Tokyo', latitude: 35.6895, longitude: 139.6917 }
+      // Add more this.locations as needed
+    ];
+    this.selectedLocation = this.locations[0];
+    this.showLocationModal=false;
     this.form = new FormGroup(formControls);
   }
   onSubmit(value:NuisanceReport){
@@ -49,5 +61,9 @@ export class CreateReportFormComponent {
     }
     let value:string = control.value || '';
    return Validators.pattern(/^\d{10}$/)(control);
+  }
+  toggleModal(){
+        console.log(this.showLocationModal);
+    this.showLocationModal=!this.showLocationModal;    
   }
 }
